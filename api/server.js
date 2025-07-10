465cd956a2493a2d29c762bc281241c90fd5281d
NadaYousryM <nada.yousry@noema.net>
2025-07-10 23:46:24 +0300

    init bookings db json server

---
+++ api/server.js
@@ -0,0 +1,33 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(jsonServer.bodyParser);
+server.use(function (req, res, next) {
+  if (req.method === "PATCH") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+server.use(middlewares);
+
+// Optional rewrites
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+
+server.use(router);
+
+server.listen(3001, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
